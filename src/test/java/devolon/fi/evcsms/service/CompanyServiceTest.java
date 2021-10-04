7b2ff03644c9006e9c2c388227664e90ee4520f8
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-04 21:40:55 +0330

    refactor CompanyServiceTest for unit testing of CompanyService and use Mockito in it.

--- src/test/java/devolon/fi/evcsms/service/CompanyServiceTest.java
+++ src/test/java/devolon/fi/evcsms/service/CompanyServiceTest.java
@@ -4,46 +4,44 @@ import devolon.fi.evcsms.mapper.CompanyMapper;
 import devolon.fi.evcsms.model.dto.CompanyDto;
 import devolon.fi.evcsms.model.entity.CompanyEntity;
 import devolon.fi.evcsms.repository.CompanyRepository;
-import devolon.fi.evcsms.service.company.CompanyService;
 import devolon.fi.evcsms.service.company.CompanyServiceImpl;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.Mockito.*;
 
 /**
  * @author Alireza Ghasabeie, a.ghasabeh@gmail.com
  */
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class CompanyServiceTest {
+
+    @Mock
     private CompanyRepository companyRepository;
 
-    @Autowired
-    public CompanyServiceTest(CompanyRepository companyRepository) {
-        this.companyRepository = companyRepository;
-    }
-
-    @MockBean
+    @Mock
     private CompanyMapper companyMapper;
+
+    @Autowired
+    @InjectMocks
+    private CompanyServiceImpl companyService;
 
     @Test
     void createACompany() {
-        CompanyService companyService = new CompanyServiceImpl(companyRepository, companyMapper);
         CompanyDto dto = CompanyDto.builder().name("TestingCompany").build();
         CompanyEntity companyEntity = new CompanyEntity();
+        companyEntity.setId(1L);
         companyEntity.setName("TestingCompany");
-        Mockito.when(companyMapper.map(dto)).thenReturn(companyEntity);
+        when(companyMapper.map(dto)).thenReturn(companyEntity);
+        when(companyRepository.save(companyEntity)).thenReturn(companyEntity);
         Long id = companyService.create(dto);
 
         Assertions.assertNotNull(id);
-        Assertions.assertEquals(1, companyRepository.count());
-    }
-
-    @AfterEach
-    void tearDown() {
-        companyRepository.deleteAll();
+        verify(companyRepository, times(1)).save(any());
     }
 }
