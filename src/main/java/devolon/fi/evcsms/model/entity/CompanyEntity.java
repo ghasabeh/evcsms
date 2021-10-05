7354584bc18c0231f4f894883322bde77fa67c63
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-05 16:38:22 +0330

    put @Talbe for CompanyEntity put unique constraint for name of company and name the constraint. put name for foreign key constraint

--- src/main/java/devolon/fi/evcsms/model/entity/CompanyEntity.java
+++ src/main/java/devolon/fi/evcsms/model/entity/CompanyEntity.java
@@ -6,12 +6,16 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
+@Table(name = "Company", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "NAME", name = "company_uk_name")
+})
 @Setter
 @Getter
 public class CompanyEntity extends BaseEntity{
     @Column(name = "NAME", nullable = false)
     private String name;
     @ManyToOne
-    @JoinColumn(name = "PARENT_ID")
+    @JoinColumn(name = "PARENT_ID", referencedColumnName = "ID",
+            foreignKey = @ForeignKey(name = "company_company_fk"))
     private CompanyEntity parent;
 }
