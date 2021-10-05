c1a05605117c74d20519e8f497a4b76dac03a70c
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-05 16:41:17 +0330

    add local_messages.properties for app put some messages map constraint name to appropriate message

--- src/main/java/devolon/fi/evcsms/EvcsmsApplication.java
+++ src/main/java/devolon/fi/evcsms/EvcsmsApplication.java
@@ -2,8 +2,10 @@ package devolon.fi.evcsms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:local_messages.properties")
 public class EvcsmsApplication {
 
     public static void main(String[] args) {
