b2455062b078c47ed6aabcf3468ca08b125b6a16
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-06 16:30:19 +0330

    use @LatitudeValidator and @LongitudeValidator in StationDto and fix use companyDto instead of CompanyEntity.

--- src/main/java/devolon/fi/evcsms/model/dto/StationDto.java
+++ src/main/java/devolon/fi/evcsms/model/dto/StationDto.java
@@ -1,6 +1,7 @@
 package devolon.fi.evcsms.model.dto;
 
-import devolon.fi.evcsms.model.entity.CompanyEntity;
+import devolon.fi.evcsms.model.dto.validation.latitude.LatitudeValidator;
+import devolon.fi.evcsms.model.dto.validation.longitude.LongitudeValidator;
 import devolon.fi.evcsms.utils.CreateValidationGroup;
 import devolon.fi.evcsms.utils.UpdateValidationGroup;
 import lombok.Data;
@@ -15,10 +16,12 @@ import javax.validation.constraints.NotNull;
 public class StationDto extends BaseDto {
     @NotBlank(message = "error.validation.notBlank", groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     private String name;
+    @LatitudeValidator(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     @NotNull(message = "error.validation.notNull", groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
-    private Double latitude;
+    private String latitude;
+    @LongitudeValidator(groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
     @NotNull(message = "error.validation.notNull", groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
-    private Double longitude;
+    private String longitude;
     @NotNull(message = "error.validation.notNull", groups = {CreateValidationGroup.class, UpdateValidationGroup.class})
-    private CompanyEntity company;
+    private CompanyDto company;
 }
