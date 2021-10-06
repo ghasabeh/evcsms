90d1e1a8c55c2a7a219e5731fe500ab53213d870
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-06 21:12:18 +0330

    handle javax.validation.ConstraintViolationException and return appropriate response to clients.
    handle MethodArgumentNotValidException for validating DTOs

--- src/main/java/devolon/fi/evcsms/utils/exception/BaseExceptionHandler.java
+++ src/main/java/devolon/fi/evcsms/utils/exception/BaseExceptionHandler.java
@@ -4,8 +4,10 @@ import devolon.fi.evcsms.model.dto.response.ResponseDto;
 import devolon.fi.evcsms.model.dto.response.ResponseType;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +16,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -41,6 +44,32 @@ public class BaseExceptionHandler {
                 .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .timestamp(new Date())
                 .message(translator.translate(ex))
+                .description(((ServletWebRequest) request).getRequest().getMethod() + " " + request.getDescription(false))
+                .build());
+        errorMessageResponseDto.setResponseType(ResponseType.EXCEPTION);
+        return errorMessageResponseDto;
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseDto<ErrorMessage> handleConstraintViolationException(MethodArgumentNotValidException ex, WebRequest request) {
+        ResponseDto<ErrorMessage> errorMessageResponseDto = new ResponseDto<>(ErrorMessage.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(new Date())
+                .message(ex.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(" , ")))
+                .description(((ServletWebRequest) request).getRequest().getMethod() + " " + request.getDescription(false))
+                .build());
+        errorMessageResponseDto.setResponseType(ResponseType.EXCEPTION);
+        return errorMessageResponseDto;
+    }
+
+    @ExceptionHandler({javax.validation.ConstraintViolationException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ResponseDto<ErrorMessage> handleConstraintViolationException(javax.validation.ConstraintViolationException ex, WebRequest request) {
+        ResponseDto<ErrorMessage> errorMessageResponseDto = new ResponseDto<>(ErrorMessage.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .timestamp(new Date())
+                .message(ex.getMessage())
                 .description(((ServletWebRequest) request).getRequest().getMethod() + " " + request.getDescription(false))
                 .build());
         errorMessageResponseDto.setResponseType(ResponseType.EXCEPTION);
