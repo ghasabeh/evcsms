6a73cdd3420d9c340b5c7685b7c3e00bd71627c3
Alireza Ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-06 03:47:01 +0330

    add equal and hashcode for BaseEntity

--- src/main/java/devolon/fi/evcsms/model/entity/BaseEntity.java
+++ src/main/java/devolon/fi/evcsms/model/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import java.util.Objects;
 
 /**
  * @author Alireza Ghasabeie, a.ghasabeh@gmail.com
@@ -18,4 +19,17 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
