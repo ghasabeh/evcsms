822a114cde42b57ac64e15d52090b72d879fd000
a.ghasabeh <a.ghasabeh@rayanbourse.ir>
2021-10-10 11:26:41 +0330

    write getAllStationsDistantFromAPointWithinRadiusOf test in order to verify nearest-location API , use test data produced with help of NATIONAL HURRICANE CENTER and CENTRAL PACIFIC HURRICANE CENTER site.

--- src/main/java/devolon/fi/evcsms/model/dto/LocationDto.java
+++ src/main/java/devolon/fi/evcsms/model/dto/LocationDto.java
@@ -2,7 +2,9 @@ package devolon.fi.evcsms.model.dto;
 
 import devolon.fi.evcsms.model.dto.validation.latitude.LatitudeValidator;
 import devolon.fi.evcsms.model.dto.validation.longitude.LongitudeValidator;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -11,6 +13,8 @@ import javax.validation.constraints.Positive;
  * @author Alireza Ghasabeie, a.ghasabeh@gmail.com
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LocationDto {
     @NotNull(message = "latitude can not be null")
     @LatitudeValidator
--- src/test/java/devolon/fi/evcsms/controller/station/StationControllerIT.java
+++ src/test/java/devolon/fi/evcsms/controller/station/StationControllerIT.java
@@ -2,6 +2,7 @@ package devolon.fi.evcsms.controller.station;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import devolon.fi.evcsms.controller.StationController;
+import devolon.fi.evcsms.model.dto.LocationDto;
 import devolon.fi.evcsms.model.dto.StationDto;
 import devolon.fi.evcsms.model.dto.response.ResponseDto;
 import devolon.fi.evcsms.model.dto.response.ResponseType;
@@ -15,6 +16,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,6 +27,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static devolon.fi.evcsms.util.JsonHelper.jsonToObject;
+import static devolon.fi.evcsms.util.JsonHelper.jsonify;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -77,6 +80,54 @@ public class StationControllerIT {
         String jsonResponse = mockMvc.perform(MockMvcRequestBuilders.get("/api/station/company/{companyId}", id)
                 .param("page", "0")
                 .param("size", "10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.responseType", is(ResponseType.GENERAL.getValue())))
+                .andReturn().getResponse().getContentAsString();
+        return jsonToObject(jsonResponse, new TypeReference<ResponseDto<List<StationDto>>>() {
+        }).getResponse();
+    }
+
+    /*
+     | Station Name | Distance From (0,0) point |
+     |--------------|---------------------------|
+     | sa1          | 14  Km                    |
+     | sa2          | 19  Km                    |
+     | sa3          | 20  Km                    |
+     | sb1          | 51  Km                    |
+     | sb2          | 76  Km                    |
+     | sc1          | 417 Km                    |
+     | sc2          | 574 Km                    |
+     | sc3          | 731 Km                    |
+     |--------------|---------------------------|
+     * This distances compute approximately from website : NATIONAL HURRICANE CENTER and CENTRAL PACIFIC HURRICANE CENTER
+     * https://www.nhc.noaa.gov/gccalc.shtml
+    **/
+    @Sql({"/company.sql", "/station.sql"})
+    @Test
+    public void getAllStationsDistantFromAPointWithinRadiusOf() throws Exception {
+        List<StationDto> stationsNearZeroWithRadius20km = callApiGetAllStationsDistantFromAPointWithinRadiusOf(new LocationDto("0.000000", "0.000000", 20D));
+        List<StationDto> stationsNearZeroWithRadius80km = callApiGetAllStationsDistantFromAPointWithinRadiusOf(new LocationDto("0.000000", "0.000000", 80D));
+        List<StationDto> stationsNearZeroWithRadius600km = callApiGetAllStationsDistantFromAPointWithinRadiusOf(new LocationDto("0.000000", "0.000000", 600D));
+
+        assertTrue(stationsNearZeroWithRadius20km.stream().map(StationDto::getName).toList()
+                .containsAll(Arrays.asList("sa1", "sa2", "sa2")));
+        assertEquals(3, stationsNearZeroWithRadius20km.size());
+
+        assertTrue(stationsNearZeroWithRadius80km.stream().map(StationDto::getName).toList()
+                .containsAll(Arrays.asList("sa1", "sa2", "sa2", "sb1", "sb2")));
+        assertEquals(5, stationsNearZeroWithRadius80km.size());
+
+        assertTrue(stationsNearZeroWithRadius600km.stream().map(StationDto::getName).toList()
+                .containsAll(Arrays.asList("sa1", "sa2", "sa2", "sb1", "sb2", "sc1", "sc2")));
+        assertEquals(7, stationsNearZeroWithRadius600km.size());
+    }
+
+    private List<StationDto> callApiGetAllStationsDistantFromAPointWithinRadiusOf(LocationDto locationDto) throws Exception {
+        String jsonResponse = mockMvc.perform(MockMvcRequestBuilders.post("/api/station/nearest-location")
+                .param("page", "0")
+                .param("size", "10")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonify(locationDto)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.responseType", is(ResponseType.GENERAL.getValue())))
                 .andReturn().getResponse().getContentAsString();
